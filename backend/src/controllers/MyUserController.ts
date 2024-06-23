14689b715f72aaae4ee7cd640262618a94fb000f
Krishna Avtar <113005173+krsnavtr-code@users.noreply.github.com>
2024-06-23 19:38:00 +0530

    Get User API

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Error getting user, please try again" });
+  }
+};
 const createCurrentUser = async (req: Request, res: Response) => {
   try {
     const { auth0Id } = req.body;
@@ -42,4 +54,4 @@ const updateCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
-export default { createCurrentUser, updateCurrentUser };
+export default { getCurrentUser, createCurrentUser, updateCurrentUser };
