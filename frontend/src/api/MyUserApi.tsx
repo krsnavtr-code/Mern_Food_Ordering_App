c46223f98148f39e1d85135012e11d59d8b4c337
Krishna Avtar <113005173+krsnavtr-code@users.noreply.github.com>
2024-06-23 18:59:50 +0530

    Update User profile request

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -12,7 +12,7 @@ type CreateUserRequest = {
 };
 
 export const useCreateMyUser = () => {
-  const {getAccessTokenSilently} = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
   const createMyUserRequest = async (user: CreateUserRequest) => {
     const accessToken = await getAccessTokenSilently();
@@ -20,7 +20,7 @@ export const useCreateMyUser = () => {
     const response = await fetch(`${API_BASE_URL}/api/my/user`, {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${accessToken}`, 
+        Authorization: `Bearer ${accessToken}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify(user),
@@ -41,3 +41,40 @@ export const useCreateMyUser = () => {
   return { createUser, isLoading, isError, isSuccess };
 };
 
+type UpdateMyUserRequest = {
+  name: string;
+  addressLine1: string;
+  // addressLine2: string;
+  city: string;
+  country: string;
+};
+export const useUpdateMyUser = () => {
+  const { getAccessTokenSilently } = useAuth0();
+
+  const updateMyUserRequest = async (formData: UpdateMyUserRequest) => {
+    const accessToken = await getAccessTokenSilently();
+    const response = await fetch(`${API_BASE_URL}/api/my/user`, {
+      method: "PUT",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to update user");
+    }
+    return response.json();
+  };
+
+  const {
+    mutateAsync: updateUser,
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+    reset,
+  } = useMutation(updateMyUserRequest);
+  return { updateUser, isLoading }; // , isSuccess, isError, error, reset
+};
